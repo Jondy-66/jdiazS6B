cfbee425ac1f4fcac8578ab2037108300dd08484
ASUS <ASUS@LAPTOP-3GC40HVK>
2024-11-06 20:41:39 -0500

    Ac vEstudiante

--- Views/vEstudiante.xaml.cs
+++ Views/vEstudiante.xaml.cs
@@ -1,9 +1,24 @@
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
 namespace jdiazS6B.Views;
 
 public partial class vEstudiante : ContentPage
 {
-	public vEstudiante()
+	private const string Url = "10.2.3.224/wsestudiantes/estudiante.php";
+	private readonly HttpClient cliente = new HttpClient();
+	private ObservableCollection<Models.Estudiante> estud;
+
+    public vEstudiante()
 	{
 		InitializeComponent();
+		Listar();
+	}
+	public async void Listar()
+	{
+		var content = await cliente.GetStringAsync(Url);
+		List<Models.Estudiante> listEst = JsonConvert.DeserializeObject<List<Models.Estudiante>>(content);
+		estud = new ObservableCollection<Models.Estudiante>(listEst);
+		lvEstudiantes.ItemsSource = estud;
 	}
 }
